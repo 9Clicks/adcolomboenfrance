8338543f5e5ece8e25e5d5ba21f5f3453bdf2a32
Alexandro F. S. Martins <contato@9clicks.com.br>
2018-09-28 12:20:12 -0300

    Configurações de Layout html inseridas.

--- resources/views/layouts/html.blade.php
+++ resources/views/layouts/html.blade.php
@@ -9,6 +9,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title></title>
 
+    @include('layouts.metas')
+
     @include('layouts.css')
 
     @include('layouts.js')
