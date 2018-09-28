86ba254a480c302997ddcb74d30eec8226555d04
Alexandro F. S. Martins <contato@9clicks.com.br>
2018-09-28 12:17:09 -0300

    JS/CSS incluido no Desenvolvimento.

--- resources/views/layouts/html.blade.php
+++ resources/views/layouts/html.blade.php
@@ -9,6 +9,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title></title>
 
+    @include('layouts.css')
+
+    @include('layouts.js')
+
 </head>
 <body>
 
---
+++ resources/views/layouts/js.blade.php
@@ -0,0 +1,6 @@
+    <script type="text/javascript" src="{{ asset('js/bootstrap.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/bundle.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/jQuery_slim.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/lightbox.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/jMask.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/validate.js') }}"></script>
