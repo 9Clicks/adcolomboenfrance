3146f258d36ea94308346288242af6543ddf5ab4
Alexandro F. S. Martins <contato@9clicks.com.br>
2018-09-28 12:13:08 -0300

    Inicio da Configuração controller principal.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,10 +1,18 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use DB;
+use App\appConfig;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public function index()
+    {
+        $app_configs           = DB::table('app_configs')->first();
+
+        return view('principal.index',[
+            'app_configs'      => $app_configs
+        ]);
+    }
 }
