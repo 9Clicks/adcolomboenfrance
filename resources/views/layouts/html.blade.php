481e7bfe199e509cbb4d9313454c8aabfb975b26
Alexandro F. S. Martins <contato@9clicks.com.br>
2018-09-28 11:27:01 -0300

    HTML Inicial criado.

---
+++ resources/views/layouts/html.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+
+<html lang="">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="icon" type="image/png" href="" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title></title>
+
+</head>
+<body>
+
+</body>
+</html>
